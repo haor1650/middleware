300b11edfb71628476fc45a593d0c9f5e4f299a9
haor1650 <1024199894@qq.com>
2017-04-17 10:09:48 +0800

    采用文件进行持久化方案4.15

---
+++ open-messaging-demo 4.15/src/main/java/io/openmessaging/demo/DefaultBytesMessage.java
@@ -0,0 +1,124 @@
+package io.openmessaging.demo;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Set;
+
+import io.openmessaging.BytesMessage;
+import io.openmessaging.KeyValue;
+import io.openmessaging.Message;
+import io.openmessaging.MessageHeader;
+
+public class DefaultBytesMessage implements BytesMessage {
+
+    private KeyValue headers = new DefaultKeyValue();
+    private KeyValue properties;
+    private byte[] body;
+
+    public DefaultBytesMessage(byte[] body) {
+        this.body = body;
+    }
+    
+	@Override
+	public String toString() {
+		return "DefaultBytesMessage [headers=" + headers + ", properties="
+				+ properties + ", body=" + Arrays.toString(body) + "]";
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(body);
+		result = prime * result + ((headers == null) ? 0 : headers.hashCode());
+		result = prime * result
+				+ ((properties == null) ? 0 : properties.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DefaultBytesMessage other = (DefaultBytesMessage) obj;
+		if (!Arrays.equals(body, other.body))
+			return false;
+		if (headers == null) {
+			if (other.headers != null)
+				return false;
+		} else if (!headers.equals(other.headers))
+			return false;
+		if (properties == null) {
+			if (other.properties != null)
+				return false;
+		} else if (!properties.equals(other.properties))
+			return false;
+		return true;
+	}
+
+	@Override public byte[] getBody() {
+        return body;
+    }
+
+    @Override public BytesMessage setBody(byte[] body) {
+        this.body = body;
+        return this;
+    }
+
+    @Override public KeyValue headers() {
+        return headers;
+    }
+
+    @Override public KeyValue properties() {
+        return properties;
+    }
+
+    @Override public Message putHeaders(String key, int value) {
+        headers.put(key, value);
+        return this;
+    }
+
+    @Override public Message putHeaders(String key, long value) {
+        headers.put(key, value);
+        return this;
+    }
+
+    @Override public Message putHeaders(String key, double value) {
+        headers.put(key, value);
+        return this;
+    }
+
+    @Override public Message putHeaders(String key, String value) {
+        headers.put(key, value);
+        return this;
+    }
+
+    @Override public Message putProperties(String key, int value) {
+        if (properties == null) properties = new DefaultKeyValue();
+        properties.put(key, value);
+        return this;
+    }
+
+    @Override public Message putProperties(String key, long value) {
+        if (properties == null) properties = new DefaultKeyValue();
+        properties.put(key, value);
+        return this;
+    }
+
+    @Override public Message putProperties(String key, double value) {
+        if (properties == null) properties = new DefaultKeyValue();
+        properties.put(key, value);
+        return this;
+    }
+
+    @Override public Message putProperties(String key, String value) {
+        if (properties == null) properties = new DefaultKeyValue();
+        properties.put(key, value);
+        return this;
+    }
+}
